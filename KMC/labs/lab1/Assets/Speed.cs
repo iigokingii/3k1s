ef429bb5d4b3dd26d2fc762e7ce2788ac62a8e50
iigokingii <smolik.valera@mail.ru>
2023-10-15 21:58:38 +0300

    added lab1 on kmc (unity)

---
+++ KMC/labs/lab1/Assets/Speed.cs
@@ -0,0 +1,22 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class NewBehaviourScript : MonoBehaviour
+{
+    // Start is called before the first frame update
+    void Start()
+    {
+
+    }
+
+    public float speed;
+    // Update is called once per frame
+    void Update()
+    {
+        float posX = transform.position.x;
+        float posY = transform.position.y;
+        float posZ = transform.position.z;
+        transform.position = new Vector3(posX + speed, posY, posZ);
+    }
+}
