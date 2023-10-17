6cd58805faf6d2096658b6bf660a7e0ed72be4a4
iigokingii <smolik.valera@mail.ru>
2023-10-17 08:12:25 +0300

    finished lab 6 on pskp

---
+++ PSKP/labs/lab5/06_02.js
@@ -0,0 +1,32 @@
+const send = require("goking");
+const http = require("http");
+const fs = require("fs");
+const url = require('url');
+
+const server = http.createServer((request,response)=>{
+    let path = url.parse(request.url).pathname;
+    if(path==="/"){
+        let html = fs.readFileSync('./index.html');
+        response.writeHead(200,{'Content-type':'text/html; charset=utf-8'});
+        response.end(html);
+    }
+    else if (path==='/send'){
+        //send jeka.pedro@mail.ru
+        //pass BiT6maeXUAEfCX35aUQG
+        //receiver smolik.valera@mail.ru
+        //message test
+        request.on('data',data=>{
+            let temp = JSON.parse(data);
+            console.log(temp);
+            send(temp.sender,temp.receiver,temp.password,temp.message);
+        })
+    }
+});
+server.listen(5000,'127.0.0.1');
+
+console.log('Server running at http://127.0.0.1:5000/');
+
+
+
+
+
