6be30a2dc92fc6f3428de0286e86205bd6d8fb58
iigokingii <smolik.valera@mail.ru>
2023-10-23 21:43:31 +0300

    lab7 on tpo done

---
+++ TPO/labs/lab7/lab7/Lab7Tests/Lab7Tests.cs
@@ -0,0 +1,209 @@
+using Lab7;
+namespace Lab7Tests
+{
+    [TestClass]
+    public class Lab7Tests
+    {
+        //#1
+        [TestMethod]
+        public void Debit_WithValidAmount_UpdatesBalance()
+        {
+            // Arrange
+            double beginningBalance = 11.99;
+            double debitAmount = 4.55;
+            double expected = 7.44;
+            BankAccount account = new BankAccount("Panchuk", beginningBalance);
+            // Act
+            account.Debit(debitAmount);
+            // Assert
+            double actual = account.Balance;
+            Assert.AreEqual(expected, actual, 0.001, "Account not debited correctly");
+        }
+        //#2
+        [TestMethod]
+        public void Debit_WhenAmountIsMoreThanBalance_ShouldThrowArgumentOutOfRange()
+        {
+            // Arrange
+            double beginningBalance = 11.99;
+            double debitAmount = 20.0;
+            BankAccount account = new BankAccount("Panchuk", beginningBalance);
+
+            // Act
+            try
+            {
+                account.Debit(debitAmount);
+            }
+            catch (ArgumentOutOfRangeException e)
+            {
+                // Assert
+                StringAssert.Contains(e.Message, BankAccount.DebitAmountExceedsBalanceMessage);
+                return;
+            }
+
+            Assert.Fail("The expected exception was not thrown.");
+        }
+        //#3
+        [TestMethod]
+        public void CreateAccount_WhenBalanceIsLessThanZero_ShouldThrowArgumentOutOfRange()
+        {
+            //Arrange
+            double beginningBalance = -20;
+            try
+            {
+                BankAccount account = new BankAccount("Panchuk", beginningBalance);
+            }
+            catch (ArgumentOutOfRangeException ex)
+            {
+                //Assert
+                StringAssert.Contains(ex.Message, BankAccount.BalanceAmountLessThanZeroMessage);
+                return;
+            }
+            Assert.Fail("The expected exception was not thrown.");
+        }
+        //#4
+        [TestMethod]
+        public void Debit_WhenDebitAmountIsLessThanZero_ShouldThrowArgumentOutOfRange()
+        {
+            // Arrange
+            double beginningBalance = 11.99;
+            double debitAmount = -20.0;
+            BankAccount account = new BankAccount("Panchuk", beginningBalance);
+
+            // Act
+            try
+            {
+                account.Debit(debitAmount);
+            }
+            catch (ArgumentOutOfRangeException e)
+            {
+                // Assert
+                StringAssert.Contains(e.Message, BankAccount.DebitAmountLessThanZeroMessage);
+                return;
+            }
+
+            Assert.Fail("The expected exception was not thrown.");
+        }
+        //#5
+        [TestMethod]
+        public void Credit_WhenCreditAmountIsLessThanZero_ShouldThrowArgumentOutOfRange()
+        {
+            // Arrange
+            double beginningBalance = 11.99;
+            double creditAmount = -20.0;
+            BankAccount account = new BankAccount("Panchuk", beginningBalance);
+
+            // Act
+            try
+            {
+                account.Credit(creditAmount);
+            }
+            catch (ArgumentOutOfRangeException e)
+            {
+                // Assert
+                StringAssert.Contains(e.Message, BankAccount.CreditAmountLessThanZeroMessage);
+                return;
+            }
+
+            Assert.Fail("The expected exception was not thrown.");
+        }
+        //#6
+        [TestMethod]
+        public void Credit_WithValidAmount_UpdatesBalance()
+        {
+            // Arrange
+            double beginningBalance = 11.99;
+            double debitAmount = 4.01;
+            double expected = 16;
+            BankAccount account = new BankAccount("Panchuk", beginningBalance);
+            // Act
+            account.Credit(debitAmount);
+            // Assert
+            double actual = account.Balance;
+            Assert.AreEqual(expected, actual, 0.001, "Account not debited correctly");
+        }
+        //#7
+        [TestMethod]
+        public void SetShortPassword_ShouldThrowArgumentOutOfRange()
+        {
+            // Arrange
+            double beginningBalance = 11.99;
+            BankAccount account = new BankAccount("Panchuk", beginningBalance);
+
+            // Act
+            try
+            {
+                account.SetPassword("qwe");
+            }
+            catch (ArgumentOutOfRangeException e)
+            {
+                // Assert
+                StringAssert.Contains(e.Message, BankAccount.PasswordIsTooShort);
+                return;
+            }
+
+            Assert.Fail("The expected exception was not thrown.");
+        }
+        //#8
+        [TestMethod]
+        public void SetEasyPassword_ShouldThrowArgumentException()
+        {
+            // Arrange
+            double beginningBalance = 11.99;
+            BankAccount account = new BankAccount("Panchuk", beginningBalance);
+
+            // Act
+            try
+            {
+                account.SetPassword("qwerty");
+            }
+            catch (ArgumentException e)
+            {
+                // Assert
+                StringAssert.Contains(e.Message, BankAccount.PasswordIsTooEasy);
+                return;
+            }
+
+            Assert.Fail("The expected exception was not thrown.");
+        }
+        //#9
+        [TestMethod]
+        public void SetNormalPassword_toBankAccount()
+        {
+            // Arrange
+            double beginningBalance = 11.99;
+            BankAccount account = new BankAccount("Panchuk", beginningBalance);
+            // Act
+            try
+            {
+                account.SetPassword("QSFrwq@2123");
+            }
+            catch (ArgumentException e)
+            {
+                // Assert
+                StringAssert.Contains(e.Message, BankAccount.PasswordIsTooEasy);
+                return;
+            }
+        }
+        //#10
+        [TestMethod]
+        public void SetLongPassword_ShouldThrowArgumentOutOfRange()
+        {
+            // Arrange
+            double beginningBalance = 11.99;
+            BankAccount account = new BankAccount("Panchuk", beginningBalance);
+            // Act
+            try
+            {
+                account.SetPassword("QSFrwq@2123222222222222222222222222222222222222222222222222222222222222222222222222222222222222222222");
+            }
+            catch (ArgumentException e)
+            {
+                // Assert
+                StringAssert.Contains(e.Message, BankAccount.PasswordIsTooLong);
+                return;
+            }
+            Assert.Fail("The expected exception was not thrown.");
+        }
+
+    }
+}
---
+++ TPO/labs/lab7/lab7/lab7/Program.cs
@@ -0,0 +1,108 @@
+﻿using System;
+using System.ComponentModel;
+
+namespace Lab7
+{
+    /// <summary>
+    /// Bank account demo class.
+    /// </summary>
+    public class BankAccount
+    {
+        private string m_customerName;
+        private double m_balance;
+        private string password;
+        public const string DebitAmountExceedsBalanceMessage = "Debit amount exceeds balance";
+        public const string DebitAmountLessThanZeroMessage = "Debit amount is less than zero";
+        public const string BalanceAmountLessThanZeroMessage = "Begining balance is less than zero";
+        public const string CreditAmountLessThanZeroMessage = "Credit amount is less than zero";
+        public const string PasswordIsTooShort = "Password is too short";
+        public const string PasswordIsTooLong = "Password is too long";
+        public const string PasswordIsTooEasy = "Password is too easy";
+
+
+        private BankAccount() { }
+
+        public BankAccount(string customerName, double balance)
+        {
+            m_customerName = customerName;
+            Balance = balance;
+        }
+        public void SetPassword(string pass)
+        {
+            Password = pass;
+        }
+        
+        public string Password
+        {
+            set
+            {
+                if (value.Length < 4)
+                {
+                    throw new ArgumentOutOfRangeException("password", value, PasswordIsTooShort);
+                }
+                if (value.Length > 22)
+                {
+                    throw new ArgumentOutOfRangeException("password", value, PasswordIsTooLong);
+                }
+                if (value == "123456789" || value == "qwerty")
+                {
+                    throw new ArgumentException("Password is too easy");
+                }
+                password = value;
+            }
+            get { return password; }
+        }
+
+        public string CustomerName
+        {
+            get { return m_customerName; }
+        }
+
+        public double Balance
+        {
+            set
+            {
+                if (value < 0)
+                {
+                    throw new ArgumentOutOfRangeException("m_balance", value, BalanceAmountLessThanZeroMessage);
+                }
+                m_balance = value;
+            }
+            get { return m_balance; }
+        }
+
+        public void Debit(double amount)
+        {
+            if (amount > m_balance)
+            {
+                throw new System.ArgumentOutOfRangeException("amount", amount, DebitAmountExceedsBalanceMessage);
+            }
+
+            if (amount < 0)
+            {
+                throw new System.ArgumentOutOfRangeException("amount", amount, DebitAmountLessThanZeroMessage);
+            }
+
+            m_balance -= amount;
+        }
+
+        public void Credit(double amount)
+        {
+            if (amount < 0)
+            {
+                throw new ArgumentOutOfRangeException("amount",amount,CreditAmountLessThanZeroMessage);
+            }
+
+            m_balance += amount;
+        }
+
+        public static void Main()
+        {
+            BankAccount ba = new BankAccount("Panchuk", 11.99);
+
+            ba.Credit(5.77);
+            ba.Debit(11.22);
+            Console.WriteLine("Current balance is ${0}", ba.Balance);
+        }
+    }
+}
